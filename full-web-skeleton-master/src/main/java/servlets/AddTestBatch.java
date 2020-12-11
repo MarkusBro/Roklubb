0ec236f06c460059c8281fa4b0e9b7705b3a6ab9
Michael <michaelhv@uia.no>
2020-12-11 13:04:59 +0100

    Removed imports

--- full-web-skeleton-master/src/main/java/servlets/AddTestBatch.java
+++ full-web-skeleton-master/src/main/java/servlets/AddTestBatch.java
@@ -1,10 +1,6 @@
 package servlets;
 
-import models.ClassResultatsModel;
 import models.TestBatchModel;
-import models.UserInfoModel;
-import org.graalvm.compiler.core.common.type.ArithmeticOpTable;
-import tools.repository.ClassRepository;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,10 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.SQLException;
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.List;
 
 @WebServlet(name = "addtestbatch", urlPatterns = {"/addtestbatch"})
 public class AddTestBatch extends HttpServlet {
@@ -29,7 +21,7 @@ public class AddTestBatch extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws IOException, ServletException {
         String id = req.getParameter("id");
-        System.out.println(",,,"+ id );
+        System.out.println(",,," + id);
 
         TestBatchModel AddtestBatch = new TestBatchModel(id);
         req.setAttribute("addtestbatch", id);
